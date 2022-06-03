80b2db74718fe3fbb41efeb7f1c281a1c177aabb
Paul Bratslavsky <paulbratslavsky@Pauls-MBP.home>
2022-06-03 13:24:26 -0400

    update url

--- pages/index.js
+++ pages/index.js
@@ -63,7 +63,8 @@ export default function Home({ posts }) {
 export async function getStaticProps() {
   // Call an external API endpoint to get posts.
   // You can use any data fetching library
-  const res = await fetch(process.env.STRAPI_URL_BASE + "/api/posts?populate=*" || "http://localhost:1337/api/posts?populate=*");
+  const url = process.env.STRAPI_URL_BASE ? `${process.env.STRAPI_URL_BASE}/api/posts?populate=*` : "http://localhost:1337/api/posts?populate=*";
+  const res = await fetch(url);
   const posts = await res.json();
 
   console.log(posts);
